df92deba87fb783a05b24a81a6a358c6355081c3
Arzor134 <106986630+Arzor134@users.noreply.github.com>
2024-02-05 23:12:32 +0300

    Add files via upload

---
+++ CV/js.js
@@ -0,0 +1,15 @@
+function playMusic() {https://music.yandex.ru/users/yamusic-my2023/playlists/197299948
+}
+
+const button = document.querySelector('button');
+// Задаем функцию, которая будет вызываться при клике на кнопку
+button.addEventListener('click', function() {
+  alert('А хорошо жить, еще лучше!');
+});
+
+
+const button = document.querySelector('button');
+// Задаем функцию, которая будет вызываться при клике на кнопку
+button.addEventListener('click', function() {
+  alert('А хорошо жить, еще лучше!');
+});
